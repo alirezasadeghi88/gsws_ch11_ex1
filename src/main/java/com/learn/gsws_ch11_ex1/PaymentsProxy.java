eb07d2d21b3cd5f87708ebe07dda70cfb20b8963
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-25 05:51:18 -0800

    R

--- src/main/java/com/learn/gsws_ch11_ex1/PaymentsProxy.java
+++ src/main/java/com/learn/gsws_ch11_ex1/PaymentsProxy.java
@@ -1,15 +1,35 @@
 package com.learn.gsws_ch11_ex1;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
-@FeignClient(name = "payments",
-url = "${name.service.url}")
-public interface PaymentsProxy {
-    @PostMapping("/payment")
-    Payment createPayment(
-            @RequestHeader String requestId,
-            @RequestBody Payment payment);
+import java.net.http.HttpHeaders;
+import java.util.UUID;
+
+@Component
+public class PaymentsProxy {
+    private final RestTemplate rest;
+    @Value("${name.service.url}")
+    private String paymentsServiceUrl;
+    public PaymentsProxy(RestTemplate rest) {
+        this.rest = rest;
+    }
+    public Payment createPayment(Payment payment) {
+        String uri = paymentsServiceUrl + "/payment";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("requestId",
+                UUID.randomUUID().toString());
+        HttpEntity<Payment> httpEntity =
+        new HttpEntity<>(payment, headers);
+        ResponseEntity<Payment> response =
+        rest.exchange(uri,
+                HttpMethod.POST,
+                httpEntity,
+                Payment.class);
+        return response.getBody();
+    }
 }
-
