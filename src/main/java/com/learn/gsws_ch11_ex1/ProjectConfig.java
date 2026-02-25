2467cb1fa85a221975da5380ba628a5d77cb7be3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-25 07:12:31 -0800

    R

--- src/main/java/com/learn/gsws_ch11_ex1/ProjectConfig.java
+++ src/main/java/com/learn/gsws_ch11_ex1/ProjectConfig.java
@@ -1,9 +1,14 @@
 package com.learn.gsws_ch11_ex1;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(
-basePackages = "com.learn")
 public class ProjectConfig {
+    @Bean
+    public WebClient webClient() {
+        return WebClient
+                .builder()
+.build();
+    }
 }
