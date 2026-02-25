5f35434b0ef30961de785618977c036ece44b2fa
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-25 05:46:51 -0800

    R

--- src/main/java/com/learn/gsws_ch11_ex1/PaymentsController.java
+++ src/main/java/com/learn/gsws_ch11_ex1/PaymentsController.java
@@ -1,30 +1,22 @@
 package com.learn.gsws_ch11_ex1;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.UUID;
-import java.util.logging.Logger;
+
 
 @RestController
 public class PaymentsController {
-    private static Logger logger =
-            Logger.getLogger(PaymentsController.class.getName());
+    private final PaymentsProxy paymentsProxy;
+    public PaymentsController(PaymentsProxy paymentsProxy) {
+        this.paymentsProxy = paymentsProxy;
+    }
     @PostMapping("/payment")
-    public ResponseEntity<Payment> createPayment(
-            @RequestHeader String requestId,
+    public Payment createPayment(
             @RequestBody Payment payment
     ) {
-        logger.info("Received request with ID " + requestId +
-                " ;Payment Amount: " + payment.getAmount());
-        payment.setId(UUID.randomUUID().toString());
-        return ResponseEntity
-.status(HttpStatus.OK)
-                .header("requestId", requestId)
-                .body(payment);
+        String requestId = UUID.randomUUID().toString();
+        return paymentsProxy.createPayment(requestId, payment);
     }
 }
