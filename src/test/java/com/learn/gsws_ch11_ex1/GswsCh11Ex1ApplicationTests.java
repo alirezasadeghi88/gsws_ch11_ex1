64d9e6269edb1dfd358bfb5edd33bbc83643b157
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-25 05:35:08 -0800

    R

---
+++ src/test/java/com/learn/gsws_ch11_ex1/GswsCh11Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.gsws_ch11_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GswsCh11Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
