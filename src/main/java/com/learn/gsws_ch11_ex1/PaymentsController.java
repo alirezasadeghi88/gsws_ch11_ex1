9591e710b8804105a9e558754197af31fc0b8b40
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-25 07:11:22 -0800

    R

--- src/main/java/com/learn/gsws_ch11_ex1/PaymentsController.java
+++ src/main/java/com/learn/gsws_ch11_ex1/PaymentsController.java
@@ -3,7 +3,7 @@ package com.learn.gsws_ch11_ex1;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.UUID;
+
 
 
 @RestController
@@ -16,7 +16,6 @@ public class PaymentsController {
     public Payment createPayment(
             @RequestBody Payment payment
     ) {
-        String requestId = UUID.randomUUID().toString();
-        return paymentsProxy.createPayment(requestId, payment);
+        return paymentsProxy.createPayment(payment);
     }
 }
