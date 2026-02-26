05218a822a30cf35a6dc1f55a9caa21fca7196e3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-26 08:06:31 -0800

    D

---
+++ src/main/java/com/learn/gsws_ch11_ex1/PurchaseRepository.java
@@ -0,0 +1,31 @@
+package com.learn.gsws_ch11_ex1;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PurchaseRepository {
+    private final JdbcTemplate jdbc;
+    public PurchaseRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+    public List<Purchase> findAllPurchases() {
+        String sql = "SELECT * FROM purchase";
+        RowMapper<Purchase> purchaseRowMapper = (r, i) -> {
+            Purchase rowObject = new Purchase();
+            rowObject.setId(r.getInt("id"));
+            rowObject.setProduct(r.getString("product"));
+            rowObject.setPrice(r.getBigDecimal("price"));
+            return rowObject;
+        };
+        return jdbc.query(sql, purchaseRowMapper);
+    }
+
+    public void storePurchase(Purchase purchase) {
+    }
+}
+
+
